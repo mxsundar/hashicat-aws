78f9afb8d98376585d76e1d9908dfeaa197d7936
mxsundar <52712252+mxsundar@users.noreply.github.com>
2021-03-30 13:31:34 -0400

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -11,7 +11,7 @@ cat << EOM > /var/www/html/index.html
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
   <center><h2>Meow World!</h2></center>
-  Hello all, Welcome to ${PREFIX}'s app. Training site from Malathy.
+  Hello all, Welcome to ${PREFIX}'s training app. Training site from Malathy.
   <!-- END -->
 
   </div>
